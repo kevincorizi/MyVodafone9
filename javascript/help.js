e9c178cbe51a16f5bb363319723f885a2f5442b2
Kevin Corizi <kevin.corizi@studenti.polito.it>
2017-07-02 13:06:07 +0200

    added answer notification, fixed style, open tabs close when open a new one

--- javascript/help.js
+++ javascript/help.js
@@ -4,7 +4,15 @@ function ExpandHelp(e) {
         text = target.textContent || text.innerText; 
 
     while ((target = target.parentElement) && !target.classList.contains("help-faq"));
-	
+
+    // Close other tabs
+    var other = document.getElementsByClassName("help-faq-open")[0];
+    if(other) {
+    	other.classList.remove("help-faq-open");
+    	other.classList.add("help-faq-closed");
+    	other.getElementsByTagName("i")[0].style.transform = "rotate(0deg)";
+    }
+
 	if(target.classList.contains("help-faq-closed")) {
 		target.classList.remove("help-faq-closed");
 		target.classList.add("help-faq-open");
@@ -14,4 +22,36 @@ function ExpandHelp(e) {
 		target.classList.remove("help-faq-open");
 		target.getElementsByTagName("i")[0].style.transform = "rotate(0deg)";
 	}
+}
+
+function VoteAnswerYes() {
+	var voteoverlay = document.getElementById("vote-answer-overlay");
+	var yespopup = document.getElementsByClassName("vote-answer-result")[0];
+	voteoverlay.style.top = "0";
+	yespopup.style.bottom = "0";
+}
+
+function VoteAnswerNo() {
+	var voteoverlay = document.getElementById("vote-answer-overlay");
+	var nopopup = document.getElementsByClassName("vote-answer-result")[1];
+	voteoverlay.style.top = "0";
+	nopopup.style.bottom = "0";
+}
+
+function CloseAnswerYes() {
+	var yespopup = document.getElementsByClassName("vote-answer-result")[0];
+	yespopup.style.bottom = "-100%";
+	setTimeout(function() {
+		var voteoverlay = document.getElementById("vote-answer-overlay");
+		voteoverlay.style.top = "100%";
+	}, 300);	
+}
+
+function CloseAnswerNo() {
+	var nopopup = document.getElementsByClassName("vote-answer-result")[1];
+	nopopup.style.bottom = "-100%";
+	setTimeout(function() {
+		var voteoverlay = document.getElementById("vote-answer-overlay");
+		voteoverlay.style.top = "100%";
+	}, 300);
 }
